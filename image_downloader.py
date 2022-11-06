512ce36f7b444a54b994155379db1d7869e242c4
Maryam Boneh <72157067+MaryamBoneh@users.noreply.github.com>
2022-11-06 10:22:24 +0330

    Deleting wrong line

--- image_downloader.py
+++ image_downloader.py
@@ -8,8 +8,6 @@ parser.add_argument('--count', help='The number of photos you want to download f
 args = parser.parse_args()
 
 response = simp.simple_image_download
-
-string = "grrks FOR grrks"
  
 # replace all instances of '،' with ',' for non-English text 
 args.imgs_name = args.imgs_name.replace("،", "," )
